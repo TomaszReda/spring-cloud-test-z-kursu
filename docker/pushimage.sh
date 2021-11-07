7e475442b24824acc6345c0e8e59d6f5ded08762
treda <tomekreda16@gmail.com>
2021-11-07 13:14:44 +0100

    Konfiguracja dockerow

---
+++ docker/pushimage.sh
@@ -0,0 +1,5 @@
+docker push 22071996/mmv2-currency-conversion-service:0.0.1-SNAPSHOT
+docker push 22071996/mmv2-naming-server:0.0.1-SNAPSHOT
+docker push 22071996/mmv2-config-server:0.0.1-SNAPSHOT
+docker push 22071996/mmv2-api-gateway:0.0.1-SNAPSHOT
+docker push 22071996/mmv2-currency-exchange-service:0.0.1-SNAPSHOT
